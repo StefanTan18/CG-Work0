1d60705070c688281990f716350a1fd43a1dee7b
Stefan Tan <stan3@stuy.edu>
2019-01-31 22:41:45 -0500

    started files

---
+++ makefile
@@ -0,0 +1,3 @@
+all:
+	gcc -o picmaker picmaker.c
+	./picmaker
---
+++ picmaker.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+  int fd;
+  int rgb[3];
+  char line[30];
+
+  fd = open("image.ppm", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+  snprintf(line, sizeof(line), "P3 512 512 255 \n");
+  write(fd,line,strlen(line));
+
+  for(int i = 0; i < 512; i++){
+    for(int j = 0; j < 512; j++){
+      rgb[0] = i%256;
+      rgb[1] = (j * i) %256;
+      rgb[2] = i%256;
+
+      sprintf(line, "%d %d %d ",rgb[0],rgb[1],rgb[2]);
+      write(fd,line,strlen(line));
+    }
+  }
+
+  close(fd);
+}
